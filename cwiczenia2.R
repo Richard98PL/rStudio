c907336aa01b3360258725f7ff9cec51b6c27113
Richard98PL <41301282+Richard98PL@users.noreply.github.com>
2021-10-23 14:04:12 +0200

    Update cwiczenia2.R

--- cwiczenia2.R
+++ cwiczenia2.R
@@ -1,43 +1,76 @@
 #cwiczenie 0
 k1 <- sample(c(1:6), 100, replace = T)
-k1b = table(k1)
-k1b
-b = barplot(k1b, col = 2, las = 1, ylim=c(0,30), main = "wykres1")
-str(b)
-text(b, max(k1b + 3), labels = k1b)
+k1wykres = tawykresle(k1)
+k1wykres
+wykres = barplot(k1wykres, col = 2, las = 1, ylim=c(0,30), main = "wykres1")
+str(wykres)
+text(wykres, max(k1wykres + 3), labels = k1wykres)
 
 k2 <- sample(c(1:6), 100, replace = T)
-k2b = table(k2)
-k2b
-c = barplot(k2b, col = 2, las = 1, ylim=c(0,30), main = "wykres2")
+k2wykres = tawykresle(k2)
+k2wykres
+c = barplot(k2wykres, col = 2, las = 1, ylim=c(0,30), main = "wykres2")
 str(c)
-text(b, max(k2b + 3), labels = k2b)
+text(wykres, max(k2wykres + 3), labels = k2wykres)
 
-M=rbind(k1b,k2b)
+M=rbind(k1wykres,k2wykres)
 
-b = barplot(M,col=c(2,3),las=1,ylim=c(0,30), main="wykres 3", beside=T, space=c(0.2,1), names.arg = c(1:6))
+wykres = barplot(M,col=c(2,3),las=1,ylim=c(0,30), main="wykres 3", beside=T, space=c(0.2,1), names.arg = c(1:6))
 str(M)
-text(b, M + 2, labels = M)
+text(wykres, M + 2, labels = M)
 
 #cwiczenie 1
 #czy granie w lotto tymi samymi numerami jest lepsze niz ciagle skreslanie nowych numerow
+length(intersect(c(0,1),c(0,2))) # test co zwraca length dla intersecta, matcha, zero czy jeden
 choose(35, 5)
-lotto <- function(kupon, n){
-  wygrane1 = c(0,0,0,0,0,0)
+
+lottoLosowe <- function(n){
+  kuponLosowy = sample(c(1:35), 5 ,replace = F)
+  wygrane = c(0,0,0,0,0,0)
   for( i in c(1:n) ){
     los = sample(c(1:35), 5 ,replace = F)
-    trafione = length(intersect(kupon,los)) + 1
-    wygrane1[trafione] = wygrane1[trafione] + 1
+    trafione = length(intersect(kupon,los))
+    wygrane[trafione + 1] = wygrane[trafione + 1] + 1
   }
-  wygr1b = wygrane1/n
-  num1=round(100*wygr1b, 1)
-  results = rbind(num1)
+  wygraneSochastyczne = wygrane/n
+  zaokragloneWynikiProcentowe=round(100*wygraneSochastyczne, 1)
+  results = rbind(zaokragloneWynikiProcentowe)
   colnames(results) = (c(0:5))
   rownames(results) = c("%")
-  b=barplot(wygr1b, col=2,las=1,ylim=c(0,1),names=c(0:5),main="Lotto")
-  text(b, wygr1b+0.2,labels=num1, xdp=T)
+  tytul = paste("Liczba losowan = ", n)
+  wykres=barplot(wygraneSochastyczne, col=2,las=1,ylim=c(0,1),names=c(0:5),main = tytul)
+  text(wykres, wygraneSochastyczne+0.2,labels=zaokragloneWynikiProcentowe)
   return (results)
 }
 
+lotto <- function(kupon, n){
+  wygrane = c(0,0,0,0,0,0)
+  for( i in c(1:n) ){
+    los = sample(c(1:35), 5 ,replace = F)
+    trafione = length(intersect(kupon,los))
+    wygrane[trafione + 1] = wygrane[trafione + 1] + 1
+  }
+  wygraneSochastyczne = wygrane/n
+  zaokragloneWynikiProcentowe=round(100*wygraneSochastyczne, 1)
+  results = rbind(zaokragloneWynikiProcentowe)
+  colnames(results) = (c(0:5))
+  rownames(results) = c("%")
+  tytul = paste("Liczba losowan = ", n)
+  wykres=barplot(wygraneSochastyczne, col=2,las=1,ylim=c(0,1),names=c(0:5),main = tytul)
+  text(wykres, wygraneSochastyczne+0.2,labels=zaokragloneWynikiProcentowe)
+  return (results)
+}
+
+
+
 kupon=c(1,2,5,10,15)
-lotto(kupon, 1000)
+liczbaLosowan = 1000
+lottoLosowe <- lottoLosowe(liczbaLosowan)
+lottoZKuponem <- lotto(kupon, liczbaLosowan)
+M=rbind(lottoLosowe, lottoZKuponem)
+M
+
+wykres = barplot(M/100,col=c(2,3),las=1,ylim=c(0,1), names = c(0:5),
+                 beside=T, space=c(0.5,2))
+
+text(wykres, 0.5, M)
